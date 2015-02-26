886bf2654f661496c9eee82dc89a8251598c1727
Chris Hudson <chris@xetius.com>
2015-02-26 17:48:05 +0000

    Passes happy path

--- src/java/com/sky/dvdstore/DvdServiceImpl.java
+++ src/java/com/sky/dvdstore/DvdServiceImpl.java
@@ -6,7 +6,7 @@ public class DvdServiceImpl implements DvdService {
 
     @Override
     public Dvd retrieveDvd(String dvdReference) throws DvdNotFoundException {
-        return null;
+        return repository.retrieveDvd(dvdReference);
     }
 
     @Override
