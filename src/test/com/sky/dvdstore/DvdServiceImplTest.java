44759119356182b128714abb8b82be9602d792f0
Chris Hudson <chris@xetius.com>
2015-02-26 18:04:51 +0000

    RetrieveDvd unknown dvd exception test

--- src/test/com/sky/dvdstore/DvdServiceImplTest.java
+++ src/test/com/sky/dvdstore/DvdServiceImplTest.java
@@ -1,15 +1,21 @@
 package com.sky.dvdstore;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class DvdServiceImplTest {
 
     private static final String VALID_DVD_REFERENCE = "DVD-TG423";
+    private static final String UNKNOWN_DVD_REFERENCE = "DVD-999";
 
     private DvdServiceImpl dvdService;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -21,5 +27,11 @@ public class DvdServiceImplTest {
     public void retrieveDvdRetrievesDvdByReferenceForValidReference() throws Exception {
         final Dvd dvd = dvdService.retrieveDvd(VALID_DVD_REFERENCE);
         assertEquals(dvd.getTitle(), "Topgun");
+    }
+
+    @Test
+    public void retrieveDvdThrowsExceptionForUnknownDvd() throws Exception {
+        expectedException.expect(DvdNotFoundException.class);
+        dvdService.retrieveDvd(UNKNOWN_DVD_REFERENCE);
     }
 }
