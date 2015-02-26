a9361898c30ebb22d92390298eaedc2e5aca7c7d
Chris Hudson <chris@xetius.com>
2015-02-26 17:24:20 +0000

    Added basic service interface and test

---
+++ src/java/com/sky/dvdstore/DvdServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sky.dvdstore;
+
+public class DvdServiceImpl implements DvdService {
+
+    @Override
+    public Dvd retrieveDvd(String dvdReference) throws DvdNotFoundException {
+        return null;
+    }
+
+    @Override
+    public String getDvdSummary(String dvdReference) throws DvdNotFoundException {
+        return null;
+    }
+}
